bf0d221073c430576b3a51e153504e7a87185667
DungLe2912 <qdung.le3912@gmail.com>
2020-03-27 22:37:19 +0700

    send data from taskform to app component

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state={
-      tasks:[]//id,name,status
+      tasks:[],//id,name,status
+      isDisplayTaskForm:false
     }
   }
   componentWillMount(){
@@ -19,30 +20,30 @@ class App extends Component {
       })
     }
   }
-  onGenerateData=()=>{
-    var tasks=[
-      {
-        id:this.randomID(),
-        name:'Action1',
-        status:true
-      },
-      {
-        id:this.randomID(),
-        name:'Action2',
-        status:false
-      },
-      {
-        id:this.randomID(),
-        name:'Action3',
-        status:true
-      }
-    ];
-    this.setState({
-      tasks:tasks
-    })
-    localStorage.setItem('tasks',JSON.stringify(tasks));
-    console.log(tasks);
-  }
+  // onGenerateData=()=>{
+  //   var tasks=[
+  //     {
+  //       id:this.randomID(),
+  //       name:'Action1',
+  //       status:true
+  //     },
+  //     {
+  //       id:this.randomID(),
+  //       name:'Action2',
+  //       status:false
+  //     },
+  //     {
+  //       id:this.randomID(),
+  //       name:'Action3',
+  //       status:true
+  //     }
+  //   ];
+  //   this.setState({
+  //     tasks:tasks
+  //   })
+  //   localStorage.setItem('tasks',JSON.stringify(tasks)); 
+  //   console.log(tasks);
+  // }
   
   s4(){
     return Math.floor((1+Math.random())*0x10000).toString(16).substring(1);
@@ -50,8 +51,28 @@ class App extends Component {
   randomID(){
     return this.s4()+'-'+this.s4()+'-'+this.s4()+'-'+this.s4()+'-'+this.s4()+'-'+this.s4();
   }
+  onHandleAddTask=()=>{
+    this.setState({
+      isDisplayTaskForm:true
+    })
+  }
+  onCloseForm=(value)=>{
+   this.setState({
+     isDisplayTaskForm:value
+   })
+  }
+  onSubmit=(data)=>{
+    var tasks=this.state.tasks;
+   data.id=this.randomID();
+   tasks.push(data);
+   this.setState({
+     tasks:tasks
+   });
+   localStorage.setItem('tasks',JSON.stringify(tasks)); 
+  }
   render() {
-    var {tasks}=this.state;
+    var {tasks,isDisplayTaskForm}=this.state;
+    var elmTaskForm= isDisplayTaskForm?<TaskForm onCloseForm={this.onCloseForm} onSubmit={this.onSubmit}/>:''
     return (
       
       <div className="container">
@@ -62,15 +83,15 @@ class App extends Component {
         
           
           <div className="row">
-            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-              <TaskForm/>
+            <div className={isDisplayTaskForm?'col-xs-4 col-sm-4 col-md-4 col-lg-4':''}>
+             {elmTaskForm}
              
             </div>
 
             
-            <div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+            <div className={isDisplayTaskForm?'col-xs-8 col-sm-8 col-md-8 col-lg-8':'col-xs-12 col-sm-12 col-md-12 col-lg-12'}>
               
-              <button type="button" className="btn btn-primary"><span className="fa fa-plus mr-5"></span> Add task</button> &nbsp;
+              <button type="button" className="btn btn-primary" onClick={this.onHandleAddTask}><span className="fa fa-plus mr-5"></span> Add task</button> &nbsp;
               <button type="button" className="btn btn-danger" onClick={this.onGenerateData}> Generate Data</button>
            
             <hr/>
--- todolist/src/components/TaskForm.js
+++ todolist/src/components/TaskForm.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import '../App.css'
 class TaskForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      name:'',
+      status:false
+
+    }
+  }
+  
+  onCloseForm(value){
+    this.props.onCloseForm(value);
+  }
+  onHandleChange=(event)=>{
+    var target=event.target;
+    var name=target.name;
+    var value=target.value;
+    if(name==='status'){
+      value=target.value==='true'?true:false;
+    }
+    this.setState({
+      [name]:value
+    })
+  }
+  onHandleSubmit=(event)=>{
+    event.preventDefault();
+    //console.log(this.state)
+    this.props.onSubmit(this.state);
+  }
     render() {
+    
         return (
             <div className="panel panel-warning">
             <div className="panel-heading">
-              <h3 className="panel-title">Add task</h3>
+              <h3 className="panel-title">Add task <span className="fa fa-remove text-right" onClick={()=>this.onCloseForm(false)}></span></h3>
             </div>
             <div className="panel-body">
              
-             <form>
+             <form onSubmit={this.onHandleSubmit}>
                <div className="form-group">
                  <label>Name task :</label>
-                 <input type="text" className="form-control"/>
+                 <input type="text" className="form-control" name="name" value={this.state.name} onChange={this.onHandleChange}/>
                </div>
               <label>Status :</label>
                
-               <select className="form-control" required="required">
-                 <option value="1">Active</option>
-                 <option value="0">Hide</option>
+               <select className="form-control" required="required" value={this.state.status} onChange={this.onHandleChange} name="status">
+                 <option value={true}>Active</option>
+                 <option value={false}>Hide</option>
                  
                </select>
                <br/>
